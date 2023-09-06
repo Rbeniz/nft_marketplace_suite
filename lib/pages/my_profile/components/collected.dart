e3772c703260ce16564291c441c21413f24c641e
Zi ~ <zi@MacBook-Pro-de-Zi.local>
2023-09-06 01:00:38 +0100

    commit

--- lib/pages/my_profile/components/collected.dart
+++ lib/pages/my_profile/components/collected.dart
@@ -1,42 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:nft_marketplace_suite/models/nft.dart';
-import 'package:nft_marketplace_suite/pages/my_profile/components/collections_section.dart';
-import 'package:nft_marketplace_suite/pages/my_profile/components/singles_section.dart';
+import '../../../models/nft.dart';
+import '../../../pages/my_profile/components/collections_section.dart';
+import '../../../pages/my_profile/components/singles_section.dart';
 
 import '../../../models/profile.dart';
-
-
 
 class Collected extends StatelessWidget {
   final String scrollkey1;
   final String scrollkey2;
   final List<NFT> nftList;
-  
-   List<Profile> listProfile= Profile.generateListProfile() ;
 
-   Collected(this.scrollkey1,this.scrollkey2, this.nftList, {Key? key}) : super(key: key);
+  List<Profile> listProfile = Profile.generateListProfile();
+
+  Collected(this.scrollkey1, this.scrollkey2, this.nftList, {Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child:Column(
+      child: Column(
         children: [
-
-        CollectionsSection(scrollkey1, listProfile),
-
-
-        ListView.separated(
-        shrinkWrap: true,
-        physics: NeverScrollableScrollPhysics(),
-          key: PageStorageKey(scrollkey2),
-          padding:  const EdgeInsets.all(20),
-          itemBuilder: (_,index)=> SinglesSection(nftList[index]), 
-          separatorBuilder: (_,index) => const SizedBox(height: 15,), 
-          itemCount: nftList.length
-          ),
-
-
-      ],),
+          CollectionsSection(scrollkey1, listProfile),
+          ListView.separated(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              key: PageStorageKey(scrollkey2),
+              padding: const EdgeInsets.all(20),
+              itemBuilder: (_, index) => SinglesSection(nftList[index]),
+              separatorBuilder: (_, index) => const SizedBox(
+                    height: 15,
+                  ),
+              itemCount: nftList.length),
+        ],
+      ),
     );
   }
 }
--- lib/pages/my_profile/components/collections_section.dart
+++ lib/pages/my_profile/components/collections_section.dart
@@ -1,103 +1,101 @@
 import 'package:flutter/material.dart';
-import 'package:nft_marketplace_suite/models/bidder.dart';
 
 import '../../../models/profile.dart';
 
-
-
-
 class CollectionsSection extends StatelessWidget {
-
   final String scrollkey;
-   Profile profile = Profile.generateProfile();
-   List<Profile> listProfile ;
+  Profile profile = Profile.generateProfile();
+  List<Profile> listProfile;
 
-  CollectionsSection(this.scrollkey,this.listProfile, {Key? key,}) : super(key: key);
- 
-
-
+  CollectionsSection(
+    this.scrollkey,
+    this.listProfile, {
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ListView.separated(
         shrinkWrap: true,
         physics: NeverScrollableScrollPhysics(),
-          key: PageStorageKey(scrollkey),
-          padding:  const EdgeInsets.all(20),
-          itemBuilder: (_,index)=>   
-              _buildIconText(listProfile[index].imgUrl!, listProfile[index].name!, listProfile[index].twitter!.substring(1),0),
-             
-          separatorBuilder: (_,index) => const SizedBox(height: 15,), 
-          itemCount: listProfile.length
-          );
+        key: PageStorageKey(scrollkey),
+        padding: const EdgeInsets.all(20),
+        itemBuilder: (_, index) => _buildIconText(
+            listProfile[index].imgUrl!,
+            listProfile[index].name!,
+            listProfile[index].twitter!.substring(1),
+            0),
+        separatorBuilder: (_, index) => const SizedBox(
+              height: 15,
+            ),
+        itemCount: listProfile.length);
   }
 }
 
-
-
 /////
-_buildIconText(String imgUrl, String title, String text , double padding ){
-    return Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-        children:[
-          Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
+_buildIconText(String imgUrl, String title, String text, double padding) {
+  return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.all(padding),
+              height: 60,
+              width: 60,
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network(
+                  imgUrl,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 20,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                      fontSize: 16, fontWeight: FontWeight.bold, height: 1.5),
+                ),
+                Row(
                   children: [
-    
-                    Container(
-                        padding: EdgeInsets.all(padding),
-                        height: 60,
-                        width: 60,
-                        decoration: BoxDecoration(
-                          color: Colors.grey[200],
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child: Image.network(
-                              imgUrl,
-                              fit: BoxFit.cover,),
-                        ),
+                    Text(
+                      text,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        color: Colors.black45,
                       ),
-    
-    
-                    const SizedBox(width: 20,),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children:  [
-                        
-                        Text(
-                          title,
-                          style: const TextStyle(
-                              fontSize: 16,
-                              fontWeight:FontWeight.bold,
-                              height: 1.5),
-                        ),
-                          Row(
-                                      children: [
-                                        Text(
-                                          text,
-                                          overflow: TextOverflow.ellipsis,
-                                          style:
-                                              const TextStyle(color: Colors.black45,),
-                                        ),
-                                        const Icon(Icons.check_circle, size: 12,)
-
-                                      ], ),
-                      ],
-                    )
-                  ],),
-                  
-                  Container(
-                    padding: EdgeInsets.all(padding),
-                    height: 60,
-                    child: const Icon(Icons.favorite_border, color: Colors.grey,)
                     ),
-
-                    
-                  
-                  ]);
-    
+                    const Icon(
+                      Icons.check_circle,
+                      size: 12,
+                    )
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
+        Container(
+            padding: EdgeInsets.all(padding),
+            height: 60,
+            child: const Icon(
+              Icons.favorite_border,
+              color: Colors.grey,
+            )),
+        const SizedBox(
+          height: 20,
+        ),
+      ]);
 }
-
